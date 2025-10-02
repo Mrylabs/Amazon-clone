50247fa0da2deec769f3459824b6cad6788519cc
mrylabs <mehryt71@gmail.com>
2025-10-02 20:32:43 +0330

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -105,11 +105,13 @@ export function loadProductsFetch() {
   });
 
   console.log('load products');
-
+  }).catch((error) => {
+    console.log('unexpected error. Please try again later.');
   });
 
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step');
@@ -131,6 +133,10 @@ export function loadProducts(fun) {
 
   fun();
 });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('unexpected error. Please try again later.');
+  });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
